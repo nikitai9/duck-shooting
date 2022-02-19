6fa7cd6b7902eb5bdfdcb2fa2fa0c96dfa7b8830
student <student@COMP-507-1-7>
2022-02-19 11:21:09 +0200

    sight

--- duck-shooting/Form1.Designer.cs
+++ duck-shooting/Form1.Designer.cs
@@ -1,4 +1,9 @@
-﻿
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
 namespace duck_shooting
 {
     partial class Form1
@@ -33,7 +38,39 @@ namespace duck_shooting
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
+
+            this.DoubleBuffered = false;
+            this.Paint += Form1_Paint;
+            this.MouseMove += Form1_MouseMove1;
+            this.Cursor.Dispose();
+
         }
+
+        private void Form1_MouseMove1(object sender, MouseEventArgs e)
+        {
+            mouse_location.X = e.Location.X;
+            mouse_location.Y = e.Location.Y;
+            this.Invalidate();
+        }
+
+        Point mouse_location = new Point();
+     
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Green, 3);
+            e.Graphics.Clear(Color.AliceBlue);
+            e.Graphics.Flush();
+            GC.Collect();
+            e.Graphics.DrawEllipse(pen,
+             new Rectangle(
+                     new Point(Location.X, Location.Y),
+                     new Size(30, 30)
+                ));
+            e.Graphics.DrawLine(pen, Location.X + 15, Location.Y - 10, Location.X + 15, Location.Y + 40);
+            e.Graphics.DrawLine(pen, Location.X - 10, Location.Y + 15, Location.X + 40, Location.Y + 15);
+
+        }
+
 
         #endregion
     }
