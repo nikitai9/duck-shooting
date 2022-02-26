6b80260ea9dc84b21694191619fbd03ee70a65d5
Ivan_pm42@student.itstep.org <Ivan_pm42@student.itstep.org>
2022-02-26 20:27:33 +0200

    Программа

---
+++ duck-shooting/C_Duck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lechiz
+{
+    class C_duck : IDisposable
+    {
+        public Image duck_img { get; private set; }
+        protected int id;
+        public Rectangle cords { get; set; }
+        public C_duck(Image duck_image, int id, int cord_duck_x, int cord_duck_y)
+        {
+            this.duck_img = duck_image;
+            this.id = id;
+            this.cords = new Rectangle(new Point(cord_duck_x, cord_duck_y), new Size(50, 50));
+
+        }
+        public void Dispose()
+        {
+            GC.Collect(GC.GetGeneration(this.duck_img));
+            GC.Collect(GC.GetGeneration(this.id));
+            GC.Collect(GC.GetGeneration(this.cords));
+        }
+    }
+
+}
--- duck-shooting/Form1.Designer.cs
+++ duck-shooting/Form1.Designer.cs
@@ -3,6 +3,10 @@ using System.Drawing;
 using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Data;
+namespace lechiz
 
 namespace duck_shooting
 {
@@ -37,37 +41,135 @@ namespace duck_shooting
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-
-            this.DoubleBuffered = false;
+            this.Text = "Duck_hunter";
+            this.TopMost = true;
             this.Paint += Form1_Paint;
             this.MouseMove += Form1_MouseMove1;
+            this.Click += Form1_Click;
+            this.duck_image = Image.FromFile("duck.png");
+
+            this.check.Size = new Size(50, 50);
+            this.check.Location = new Point(50, 50);
+            this.Controls.Add(check);
             this.Cursor.Dispose();
 
+            this.duck_position.X = 350;
+            this.duck_position.Y = 350;
+            this.duck_position.Width = 50;
+            this.duck_position.Height = 50;
+
+            this.DoubleBuffered = true;
         }
 
-        private void Form1_MouseMove1(object sender, MouseEventArgs e)
+        private void Form1_Click(object sender, EventArgs e)
         {
-            mouse_location.X = e.Location.X;
-            mouse_location.Y = e.Location.Y;
-            this.Invalidate();
+            cord_point.Add(new Point(Cursor.Position.X + 10, Cursor.Position.Y + 10));
+            duck_colection.Add(new C_duck(duck_image, 1, rand_duck.Next(0, 500), rand_duck.Next(0, 500)));
+
         }
+
+        Random rand_duck = new Random();
+        Point elipse_location = new Point();
+        Rectangle duck_position = new Rectangle();
+        Rectangle shut_position = new Rectangle();
+        Image duck_image;
+
 
         Point mouse_location = new Point();
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Pen pen = new Pen(Color.Green, 3);
-            Pen pens = new Pen(Color.Green, 3);
             e.Graphics.Flush();
             GC.Collect();
-            e.Graphics.DrawEllipse(pens,
-                mouse_location.X, mouse_location.Y, 30, 30);
-            e.Graphics.DrawLine(pen, mouse_location.X + 15, mouse_location.Y - 10, mouse_location.X + 15, mouse_location.Y + 40);
-            e.Graphics.DrawLine(pen, mouse_location.X - 10, mouse_location.Y + 15, mouse_location.X + 40, mouse_location.Y + 15);
-        } 
+            DrawImage2FloatRectF(e);
+            Pen pen = new Pen(Color.Green, 5);
+
+            e.Graphics.DrawLine(pen, elipse_location.X + 30, elipse_location.Y + 20, elipse_location.X + 60, elipse_location.Y + 20);
+            e.Graphics.DrawLine(pen, elipse_location.X + 10, elipse_location.Y + 20, elipse_location.X - 20, elipse_location.Y + 20);
+            e.Graphics.DrawLine(pen, elipse_location.X + 20, elipse_location.Y + 30, elipse_location.X + 20, elipse_location.Y + 60);
+            e.Graphics.DrawLine(pen, elipse_location.X + 20, elipse_location.Y + 10, elipse_location.X + 20, elipse_location.Y - 20);
+            e.Graphics.DrawEllipse(Pens.Red,
+                elipse_location.X, elipse_location.Y, 40, 40);
+
+            foreach (Point point in cord_point)
+            {
+                e.Graphics.FillEllipse(Brushes.Red,
+                point.X, point.Y, 20, 20);
+                shut_position.X = point.X;
+                shut_position.Y = point.Y;
+                shut_position.Width = 20;
+                shut_position.Height = 20;
+            }
+        }
+
+        private void Form1_MouseMove1(object sender, MouseEventArgs e)
+        {
+            elipse_location.X = e.Location.X;
+            elipse_location.Y = e.Location.Y;
+            this.Invalidate();
+        }
+
+        public void DrawImage2FloatRectF(PaintEventArgs e)
+        {
+            foreach (C_duck duck in duck_colection)
+            {
+                if (check_kill(duck) == true)
+                {
+                    duck_colection.Remove(duck);
+                    break;
+                }
+                e.Graphics.DrawImage(duck.duck_img, duck.cords);
+            }
+            if (duck_colection.Count >= MAX_DUCK)
+            {
+                MakeScreenshot();
+                MessageBox.Show("END GAME");
+                this.Close();
+            }
+
+            GC.Collect();
+        }
+
+        public void MakeScreenshot()
+        {
+            string names = DateTime.Now.ToString().Replace(".", "_").Replace(":", "_");
+            names += ".jpg";
+            Rectangle bounds = new Rectangle(this.Location.X, this.Location.Y, this.Size.Width, this.Size.Height);
 
 
+            using (var bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (var g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(this.Location, Point.Empty, bounds.Size);
+                }
+                bitmap.Save(names, ImageFormat.Jpeg);
+            }
+        }
+        void new_duck()
+        {
+            this.duck_position.X = duck_position.X + 100;
+            this.duck_position.Y = duck_position.Y + 100;
+            //isAlive = true;
+
+        }
+
+        bool check_kill(C_duck duck)
+        {
+
+            if (shut_position.IntersectsWith(duck.cords))
+            {
+                MessageBox.Show("Kill duck!");
+                GC.Collect(GC.GetGeneration(duck));
+                return true;
+            }
+            return false;
+        }
+
+        readonly int MAX_DUCK = 10;
+        List<Point> cord_point = new List<Point>();
+        List<C_duck> duck_colection = new List<C_duck>();
+        Label check = new Label();
         #endregion
     }
 }
